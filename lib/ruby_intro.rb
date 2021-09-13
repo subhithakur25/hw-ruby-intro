27cd54c1be884bfbd6f2100beb12e25c220c9036
EC2 Default User <ec2-user@ip-172-31-47-224.us-east-2.compute.internal>
2021-09-13 02:59:12 +0000

    logs_code_part1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,41 @@
 
 def sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  else
+    sum = 0
+    for i in arr
+      sum += i
+    end
+    return sum
+  end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  end
+  if arr.length == 1
+    return arr[0]
+  end
+return arr.sort()[-1] + arr.sort()[-2]
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+if arr.length <= 1
+  return false
+end
+for a in 0..arr.length-1 do
+  for b in a+1..arr.length-1 do
+    if arr[a] + arr[b] == n
+      return true
+    end
+  end
+end
+return false
 end
 
 # Part 2
