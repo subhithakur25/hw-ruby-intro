43fd32b3bcb4563eedbcbca61d7860e017232eb1
EC2 Default User <ec2-user@ip-172-31-47-224.us-east-2.compute.internal>
2021-09-13 04:19:56 +0000

    logs_code_part2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -45,14 +45,46 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.length == 0
+    return false
+  end
+  start = s.to_s[0].upcase
+  if start == "A" || start == "E" || start == "I" || start == "O" || start == "U"
+    return false
+  end
+  if start.match?(/[[:alpha:]]/)
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.length == 0
+    return false
+  end
+  for i in 0..s.length-1 do
+    if s[i]!= '0' && s[i]!= '1'
+     return false
+    end
+  end
+  d=0
+  a=1
+  len = s.length-1
+  while len >= 0 do
+    d=d+(s[len].to_i)*a
+    a=a*2
+    len -= 1
+  end
+  if d%4 == 0
+    return true
+  end
+  return false
 end
 
 # Part 3
